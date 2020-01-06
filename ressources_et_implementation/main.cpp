1d2db7900f04cc68ecb0cbe51c85ce52e9e4f693
lazlo barragan <lazlo.barragan@gmail.com>
2020-01-06 14:17:30 +0100

    Update main.cpp

--- ressources_et_implementation/main.cpp
+++ ressources_et_implementation/main.cpp
@@ -26,16 +26,16 @@ void add_note(vector<double>& signal, double freq_note, double duration, double 
 int main(int argc, char* argv[], char* env[]){
 
 	// QUESTION 1:
-	/*
+	
 	string output = "la_question_1.wav";
 	vector<double> signal;// = read_signal("sounds/GammePiano.wav", 1, 44100);
 	add_note(signal, 440, 6, 44100);
 	write_signal(output, signal, 1, 44100);
-	*/
+	
 
 
 	//QUESTION 2: gamme
-	/*
+	
 	vector<double> notes2;
 	notes2.push_back(DO3);
 	notes2.push_back(RE3);
@@ -46,34 +46,34 @@ int main(int argc, char* argv[], char* env[]){
 	notes2.push_back(SI3);
 
 	genereGamme(5, 44100, 1, notes2, "gammeQ2.wav");
-	*/
+	
 
 
 	//QUESTION 2: accord
-	/*
+	
 	vector<double> notes1;
 	notes1.push_back(LA3);
 	notes1.push_back(DO3);
 	notes1.push_back(MI3);
 
 	genereAccord(15, 44100, 1, notes1, "accordQ2.wav");
-	*/
+	
 
 	//QUESTION 4: Filtres
-	/*
+	
 	string file = "sounds/Whistle.wav";
 	int freqEch = 8000;
 	vector<double> signal = read_signal(file, 1);
 
 	passe_bas(freqEch, 950.0, 1, signal, "filtre_passe_bas.wav");
-	*/
+	
 
 	//QUESTION 5: Butterworth
-	/*
-	string file = "gammeQ2.wav";
-	int freqEch = 44100;
-	vector<double> signal = read_signal(file, 1);
-	butterworth(150, freqEch, signal, "butterworth.wav");
-	*/
+	
+	string file2 = "gammeQ2.wav";
+	int freqEch2 = 44100;
+	vector<double> signal2 = read_signal(file2, 1);
+	butterworth(150, freqEch2, signal2, "butterworth.wav");
+	
 	return 0;
 }
